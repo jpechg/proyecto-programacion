03248052427d01584eebfdb4121a387e37bb948e
Juan P <jpechg@gmail.com>
2026-04-13 02:17:53 +0200

    arreglar un par de errores con realloc y el sscanf, y modificar la funcion para devolver el numero de lineas

--- reader.c
+++ reader.c
@@ -11,7 +11,7 @@ typedef struct {
   int dia_semana;
   int t0; //numero de minutos desde las 00:00
   int tf;
-  char actividad[128]; //TODO: seria ideal usar un enum, ya que hace un uso mas eficiente de la memoria
+  char actividad[128]; //TODO: seria ideal usar un int, ya que hace un uso mas eficiente de la memoria
   char centro[128];
   int total;
   int ocupado;
@@ -19,13 +19,13 @@ typedef struct {
   char tipo[128];
 } actividad;
 
-actividad *read_csv(char *filename);
+actividad *read_csv(char *filename, unsigned int *lineas);
 
-actividad *read_csv(char *filename) {
+actividad *read_csv(char *filename, unsigned int *lineas) {
   FILE *csv = fopen(filename, "r");
   actividad *dataptr = malloc(sizeof(actividad)*SIZE_INCREMENT);
   char linea[2048];
-  char dia_semana_str[64];
+  char dia_semana_str[16];
   int h0, hf, m0, mf; //variables locales intermedias para poder leer el archivo
   unsigned int n = 0; //como el tamaño del archivo es de en torno a 5k lineas, nos vale de sobra con un unsigned int (64k)
   unsigned int capacidad = SIZE_INCREMENT;
@@ -39,16 +39,17 @@ actividad *read_csv(char *filename) {
   fgets(linea, sizeof(linea), csv); //quitamos la primera linea que no contiene información
   while(fgets(linea, sizeof(linea), csv)) {
     if (n >= capacidad) {
-      actividad *tempptr = realloc(dataptr, capacidad*sizeof(actividad));
       capacidad = capacidad * 2; //asi el resizing de memoria se hace en O(log(n)), gastando menos operaciones que una suma O(n)
+      actividad *tempptr = realloc(dataptr, capacidad*sizeof(actividad));
       if (tempptr == NULL) {
         printf("problema de resizing del buffer");
         free(dataptr);
         return NULL;
       }
+      dataptr = tempptr;
     }
     
-    sscanf(linea, "%d %d %d %15s %d:%d %d:%d %127s %127s %d %d %d %127s", 
+    sscanf(linea, "%d %d %d %15[^ ] %d:%d %d:%d %127[^ ] %127[^ ] %d %d %d %127s[^\n]", 
         &dataptr[n].year, 
         &dataptr[n].mes,
         &dataptr[n].dia,
@@ -64,7 +65,7 @@ actividad *read_csv(char *filename) {
         &dataptr[n].libre, 
         dataptr[n].tipo);
 
-    dataptr[n].t0 = h0*60 + m0;
+    dataptr[n].t0 = h0*60 + m0; //calculamos el tiempo en minutos sumando horas y minutos del dato capturado
     dataptr[n].tf = hf*60 + mf;
 
     for (int i = 0; i < 7; i++) {
@@ -76,12 +77,15 @@ actividad *read_csv(char *filename) {
     n++;
     printf("%s", linea);
   }
+  *lineas = n; //devolvemos el numero de lineas que se han escrito
+
   fclose(csv);
   return(dataptr);
 }
 
 int main(void) {
-  read_csv("dataset_muestra.csv");
-//  printf("%li", sizeof(actividad));
+  unsigned int l = 0;
+  read_csv("dataset_muestra.csv", &l);
+  printf("%i", l);
   return 0;
 }
