b0ec03be871cd9af9568bf7d43be1eb2bbad473e
hugoserems <hugoserems@gmail.com>
2026-04-30 18:09:55 +0200

    Update utilidades.h

--- utilidades.h
+++ utilidades.h
@@ -3,6 +3,6 @@
 int actividad_ocupada(actividad *dataptr, unsigned int i);
 int add_favoritos(actividad *dataptr,uint32_t valor,unsigned int n_datos);
 int longitud_favoritos();
-int leer_favoritos();
+actividad *leer_favoritos();
 int eliminar_favoritos(uint32_t valor);
 int actividad_popular(actividad *dataptr, unsigned int lineas, uint32_t c);
